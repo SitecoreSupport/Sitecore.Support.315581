9d4ec62ae13b61e559ee1eb866bda2edbf5dfd4d
Tyler Jass <tja@sitecore.net>
2019-03-07 12:21:20 -0500

    Initial commit for patch

---
+++ src/Sitecore.Support.315581/Owin/Authentication/Pipelines/CookieAuthentication/SignedIn/UpdateStatistics.cs
@@ -0,0 +1,32 @@
+﻿namespace Sitecore.Support.Owin.Authentication.Pipelines.CookieAuthentication.SignedIn
+{
+  using System;
+  using Sitecore.Diagnostics;
+  using Sitecore.Owin.Authentication.Identity;
+  using Sitecore.Owin.Authentication.Pipelines.CookieAuthentication.SignedIn;
+
+  public class UpdateStatistics : SignedInProcessor
+  {
+    private IMembership _membership;
+
+    public UpdateStatistics(IMembership membership)
+    {
+      Assert.ArgumentNotNull(membership, nameof(membership));
+
+      _membership = membership;
+    }
+
+    public override void Process(SignedInArgs args)
+    {
+      var user = _membership.GetUser(args.User.UserName);
+      if (user == null)
+      {
+        return;
+      }
+
+      // here we can change  user last login statistics and so on.
+      user.LastLoginDate = DateTime.Now;
+      _membership.UpdateUser(user);
+    }
+  }
+}
---
+++ src/Sitecore.Support.315581/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.315581")]
+[assembly: AssemblyProduct("Sitecore.Support.315581")]
+[assembly: ComVisible(false)]
